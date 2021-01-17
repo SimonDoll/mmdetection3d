7e3d1f08bcf2077d8d53bdc09bf504d627bce74b
Simon D <simondoll.studium@gmail.com>
2021-01-17 18:53:35 +0000

    adds prev image loading

--- mmdet3d/datasets/pipelines/__init__.py
+++ mmdet3d/datasets/pipelines/__init__.py
@@ -53,4 +53,5 @@ __all__ = [
     "AugmentPointsWithCurrentImageFeatures",
     "AugmentPointsWithCorrespondingImageFeatures",
     "LoadPrevPointsFromFile",
+    "LoadPrevMultiViewImagesFromFile",
 ]
